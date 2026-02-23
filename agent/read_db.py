fc9789ba14dae0af702e74a0fd08fb5f8224c6b1
dvdgamer2003 <divyeshravane21543@gmail.com>
2026-02-24 01:47:36 +0530

    chore: Remove venv, node_modules and debug files from git tracking

--- agent/read_db.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-
-db = MongoClient('mongodb+srv://divyeshshirude:QOQ0XjJpUoE3zOaI@cluster0.a40p2.mongodb.net/ci-cd-agent?retryWrites=true&w=majority&appName=Cluster0').get_database()
-runs = list(db.runresults.find().sort('start_time', -1).limit(4))
-for r in runs:
-    print(r.get('status'), r.get('currentStep'), r.get('runId'))
-    print(r.get('logs'))
-    print("---")
